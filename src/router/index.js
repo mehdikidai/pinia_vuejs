a4e45671ed280f1cbfc1f4e468d5d9f650f8b6f7
mehdi_kidai <mehdikidai@gmail.com>
2024-09-11 13:37:06 +0100

    vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import AdminView from '@/views/AdminView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import AdminView from "@/views/AdminView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'admin',
-      component: AdminView
-    },
-    {
-      path: '/login',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/LoginView.vue')
-    }
-  ]
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "admin",
+            component: AdminView,
+            beforeEnter: (to, from, next) => {
+                next({ name: "login" });
+            },
+        },
+        {
+            path: "/login",
+            name: "login",
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import("../views/LoginView.vue"),
+        },
+    ],
+});
 
-export default router
+export default router;
